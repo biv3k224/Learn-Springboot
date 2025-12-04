d79ef7c7d29c0d8e0fd1521ec8ecf2059106f3a6
Bibek Kumar Tamang <bibek@MacBookPro.lan>
2025-12-03 23:57:39 -0500

    Learn about API endpoints

---
+++ demo/src/main/java/com/example/demo/HelloApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApiServiceApplication.class, args);
+	}
+
+}
